f988133e8d14a7f3ff63ddbe0c371eafbd0027cf
Andre Rodrigues <andre.silva96@live.com>
2018-01-04 23:52:53 -0300

    Listar usuarios

---
+++ src/app/users/user-list/user-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {JwtTokenService} from '../../services/jwt-token.service';
+import {Headers, RequestOptions} from '@angular/http';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+  users: Array<Object> = [];
+  constructor(private http: HttpClient, private jwtToken: JwtTokenService) { }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+      const headers = new HttpHeaders();
+      headers.set('Authorization', `Bearer ${this.jwtToken.getToken()}`);
+      headers.set('Content-Type', 'application/json');
+      console.log(this.jwtToken.getToken());
+      this.http
+          .get<Array<Object>>('http://localhost:8080/api/users', {headers})
+          .subscribe(response => console.log(response));
+  }
+}
